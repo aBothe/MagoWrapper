e72c1268e87e321149157d82a98a49db5e85a84f
Michael Crompton <michaelc37@msn.com>
2014-02-23 17:42:12 -0500

    conversion of project files to vs2012

--- DebugEngine/MagoNatDE/MagoNatDE_i.h
+++ DebugEngine/MagoNatDE/MagoNatDE_i.h
@@ -4,9 +4,9 @@
 
 
  /* File created by MIDL compiler version 8.00.0595 */
-/* at Sat Feb 02 14:24:55 2013
+/* at Sun Feb 23 14:30:37 2014
  */
-/* Compiler settings for .\MagoNatDE.idl:
+/* Compiler settings for MagoNatDE.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.00.0595 
     protocol : dce , ms_ext, c_ext, robust
     error checks: allocation ref bounds_check enum stub_data 
